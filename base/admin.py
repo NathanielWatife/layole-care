fffdb953b2471cde7bc6b48affc3ff09f5de673f
NathanielWatife <awosanminathaniel0@gmail.com>
2024-04-23 05:40:30 +0100

    Changese made to all files

--- base/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Appointment, Contact
-
-# Register your models here.
-admin.site.register(Appointment)
-admin.site.register(Contact)
-# admin.site.register(Newsletter)
