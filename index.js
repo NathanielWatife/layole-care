0e5a4fe5f3295ea70c8e864ece52505343a956cc
NathanielWatife <awosanminathniel0@gmail.com>
2024-08-16 12:26:05 +0100

    Change to module

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const path = require("path");
 const nodemailer = require("nodemailer");
+const { request } = require("http");
 
-require('dotenv').config();
+dotenv.config();
 
 // database connection
 mongoose.connect(process.env.MONGO_URL)
@@ -25,36 +26,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // POST endpoint to handle form submission
-app.post('/send-email', (req, res) => {
+app.post('/send-email', async (req, res) => {
   const { name, phone, email, message } = req.body;
 
   // Create a transporter object using SMTP transport
   const transporter = nodemailer.createTransport({
-    service: 'Gmail', // or use another email service
+    service: 'Gmail',
+    // host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // or use another email service
     auth: {
-      user: 'your-email@gmail.com',
-      pass: 'your-email-password'
+      user: 'EMAIL_USER',
+      pass: 'EMAIL_PASS'
     }
   });
-
+ 
   // Setup email data
   const mailOptions = {
-    from: email,
-    to: 'your-email@gmail.com',
+    from:email,
+    to: 'devnathy022@gmail.com',
     subject: 'New Appointment Request',
     text: `Name: ${name}\nPhone: ${phone}\nEmail: ${email}\nMessage: ${message}`
   };
 
   // Send email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error(error);
-      return res.status(500).send('Error sending email.');
-    }
-    res.status(200).send('Email sent successfully!');
-  });
+  try {
+    const info =  await transporter.sendMail(mailOptions);
+    console.log("Email sent", info.response);
+    res.status(200).send("Email has been successfully sent");
+  } catch (err) {
+    console.log('Email not sent', err);
+    res.status(500).send("Email not successfully sent!!");
+  }
+
+  // transporter.sendMail(mailOptions, (error, info) => {
+  //   if (error) {
+  //     console.error(error);
+  //     return res.status(500).send('Error sending email.');
+  //   }
+  //   res.status(200).send('Email sent successfully!');
+  // });
 });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
